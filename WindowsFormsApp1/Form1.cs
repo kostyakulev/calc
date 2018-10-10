af71794565c6de33cef55c9df2e80170e79b44b7
OnMyOwn <kostyakylev@gmail.com>
2018-10-10 09:22:41 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class kalculyator : Form
+    {
+        public kalculyator()
+        {
+            InitializeComponent();
+        }
+        double a, b;
+        int count;
+        bool znak = true;
+       
+        private void calculate()
+        {
+
+            switch (count)
+            {
+                case 1:
+                    b = a + double.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                case 2:
+                    b = a - double.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a * double.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a / double.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 5:
+                    b = (double) Math.Pow(a, double.Parse(textBox1.Text));
+                    textBox1.Text = b.ToString();
+                    break;
+                case 6:
+                    b = (double) Math.Sqrt(a);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 7:
+                    b = (double)Math.Sin(a);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 8:
+                    b = (double)Math.Cos(a);
+                    textBox1.Text = b.ToString();
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+            
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            calculate();
+            Label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+            
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            if ((Label1.Text.IndexOf(",") == -1) && (Label1.Text.IndexOf("∞") == -1))
+                textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Label1.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if (znak == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                znak = false;
+            }
+            else if (znak == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                znak = true;
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            Label1.Text = a.ToString() + "+";
+            znak = true;
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            Label1.Text = a.ToString() + "-";
+            znak = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            Label1.Text = a.ToString() + "*";
+            znak = true;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            Label1.Text = a.ToString() + "/";
+            znak = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ")";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "(";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+            
+        }
+                private void button21_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 5;
+            Label1.Text = a.ToString() + "^";
+
+            znak = true;
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 6;
+            Label1.Text = a.ToString() + "√";
+            znak = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 7;
+            Label1.Text = a.ToString() + "sin";
+            znak = true;
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 8;
+            Label1.Text = a.ToString() + "cos";
+            znak = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+            
+        }
+    }
+}
